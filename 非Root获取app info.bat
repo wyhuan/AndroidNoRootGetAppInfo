2f4c9fea904f3d4e6410986f48b9b85adb960492
neil <wyh471738293@sina.com>
2016-07-01 13:14:03 +0800

    批处理修改

--- 非Root获取app info.bat
+++ 非Root获取app info.bat
@@ -4,6 +4,6 @@ title getnorootinfo
 echo ---------------------------
 echo click backup button
 echo ---------------------------
-adb backup -nosystem -apk com.tencent.utest.recorder
+adb backup -nosystem -apk com.xx.xx
 java -jar abe.jar unpack backup.ab backup.tar
 exit
